10387cfd054e5eed06a5b111d9ed0902896887a0
Yitzchok Kimelman <itchkimel@gmail.com>
2021-06-09 16:27:00 -0400

    commit tree f76f1d53c31ab7b6dc2b120c0ffa9a394a04ae92

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+  validates :title, presence: true 
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion:  %w(Fiction Non-Fiction) 
+  validate :clickbait?
+  
+  # I looked at the soloution for the below code 
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
